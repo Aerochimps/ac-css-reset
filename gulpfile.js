54ab336407aac48a97008bec5d67080392d4f588
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2016-12-13 00:25:23 +0100

    Add Gulp
    - package.json with al gulp dependencies;
    - gulpfile.js
    --- sass task;
    --- watch task;
    --- dist task;

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+//Sass variables
+var input = './sass/*.scss';
+var output = './css/';
+var dist = './dist/';
+var sassOptions = {
+	errLogToConsole: true,
+	outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+	return gulp
+	.src(input)
+	.pipe(sourcemaps.init())
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(sourcemaps.write())
+	.pipe(autoprefixer())
+	.pipe(gulp.dest(output))
+});//End task sass
+
+gulp.task('watch', function() {
+	return gulp
+	.watch(input, ['sass'])
+	.on('change', function(event) {
+	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
+});//End tastk watch
+
+gulp.task('dist', function () {
+	return gulp
+	.src(input)
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(minify())
+	.pipe(gulp.dest(dist))
+});//End taslk dist
+
+gulp.task('default', ['sass','watch']);
